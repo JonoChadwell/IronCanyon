849a8c45c4e4b76b487527ffbd726e4ae0f84a86
michael-rutledge <mrutledge2808@gmail.com>
2017-02-16 15:07:36 -0800

    no more turret segfault

--- IronCanyon/main.cpp
+++ IronCanyon/main.cpp
@@ -551,7 +551,7 @@ static void render()
 static void updateWorld()
 {
 	if (!dead && !gamePaused) {
-        //Turret::quadtree = quadtree;
+        Turret::quadtree = quadtree;
 		double timePassed = thisFrameStartTime - lastFrameStartTime;
         while (timePassed > maxPhysicsStepLength) {
             timePassed -= maxPhysicsStepLength;
