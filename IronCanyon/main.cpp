1cd781b5a5e150a87481925ba6a9f89d4447b0ac
zrose <zrose@DESKTOP-7VRUATC>
2017-02-15 21:25:34 -0800

    Jumping over enemies doesn't cause game over.

--- IronCanyon/main.cpp
+++ IronCanyon/main.cpp
@@ -342,7 +342,7 @@ static void stepGameObjects(float dt) {
 	vector<GameObject *> qObjects;
 	quadtree->getObjects(player->xpos, player->zpos, &qObjects);
 	for (unsigned int i = 0; i < qObjects.size(); i++) {
-		if (dynamic_cast<Enemy*>(qObjects[i]) != NULL && distance(vec2(player->xpos, player->zpos), vec2(qObjects[i]->pos.x, qObjects[i]->pos.z)) < 2) {
+		if (dynamic_cast<Enemy*>(qObjects[i]) != NULL && distance(vec3(player->xpos, player->zpos, player->ypos), vec3(qObjects[i]->pos.x, qObjects[i]->pos.z, qObjects[i]->pos.y)) < 2) {
 			// Game over
 			glfwSetWindowShouldClose(window, GL_TRUE);
 		}
