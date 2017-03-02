09f199020e52a7bd8bc456d0e9be41f9f20b7cc9
JonoChadwell <jono.chadwell@gmail.com>
2017-03-02 15:25:58 -0800

    Added download script

---
+++ download_assets.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p resources/drive
+cd resources/drive
+
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/alien2.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/ore01.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/ore02.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/ore03.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/plant01.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/plant02.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/rockstack01.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/rockstack02.obj
+wget https://s3-us-west-2.amazonaws.com/iron-canyon/wall01.obj
+
