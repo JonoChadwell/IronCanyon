1760e4aa57e5520de93c41a183d75e32a82be46a
theDumb12 <theDumb12@yahoo.com>
2017-03-14 12:24:03 -0700

    VFC changes

--- IronCanyon/VFC.cpp
+++ IronCanyon/VFC.cpp
@@ -27,45 +27,45 @@ VFC* VFC::ExtractVFPlanes(VFC* vfc) {
 	vec3 n; //use to pull out normal
 	float l; //length of normal for plane normalization
 
-	vfc->Left.x = 0; // see handout to fill in with values from comp
-	vfc->Left.y = 0; // see handout to fill in with values from comp
-	vfc->Left.z = 0; // see handout to fill in with values from comp
-	vfc->Left.w = 0; // see handout to fill in with values from comp
+	vfc->Left.x = comp[0][3] + comp[0][0]; // see handout to fill in with values from comp
+	vfc->Left.y = comp[1][3] + comp[1][0]; // see handout to fill in with values from comp
+	vfc->Left.z = comp[2][3] + comp[2][0]; // see handout to fill in with values from comp
+	vfc->Left.w = comp[3][3] + comp[3][0]; // see handout to fill in with values from comp
 	vfc->planes[0] = vfc->Left;
 	//cout << "Left' " << Left.x << " " << Left.y << " " << Left.z << " " << Left.w << endl;
 
-	vfc->Right.x = 0; // see handout to fill in with values from comp
-	vfc->Right.y = 0; // see handout to fill in with values from comp
-	vfc->Right.z = 0; // see handout to fill in with values from comp
-	vfc->Right.w = 0; // see handout to fill in with values from comp
+	vfc->Right.x = comp[0][3] - comp[0][0]; // see handout to fill in with values from comp
+	vfc->Right.y = comp[1][3] - comp[1][0]; // see handout to fill in with values from comp
+	vfc->Right.z = comp[2][3] - comp[2][0]; // see handout to fill in with values from comp
+	vfc->Right.w = comp[3][3] - comp[3][0]; // see handout to fill in with values from comp
 	vfc->planes[1] = vfc->Right;
 	//cout << "Right " << Right.x << " " << Right.y << " " << Right.z << " " << Right.w << endl;
 
-	vfc->Bottom.x = 0; // see handout to fill in with values from comp
-	vfc->Bottom.y = 0; // see handout to fill in with values from comp
-	vfc->Bottom.z = 0; // see handout to fill in with values from comp
-	vfc->Bottom.w = 0; // see handout to fill in with values from comp
+	vfc->Bottom.x = comp[0][3] + comp[0][1]; // see handout to fill in with values from comp
+	vfc->Bottom.y = comp[1][3] + comp[1][1]; // see handout to fill in with values from comp
+	vfc->Bottom.z = comp[2][3] + comp[2][1]; // see handout to fill in with values from comp
+	vfc->Bottom.w = comp[3][3] + comp[3][1]; // see handout to fill in with values from comp
 	vfc->planes[2] = vfc->Bottom;
 	//cout << "Bottom " << Bottom.x << " " << Bottom.y << " " << Bottom.z << " " << Bottom.w << endl;
 
-	vfc->Top.x = 0; // see handout to fill in with values from comp
-	vfc->Top.y = 0; // see handout to fill in with values from comp
-	vfc->Top.z = 0; // see handout to fill in with values from comp
-	vfc->Top.w = 0; // see handout to fill in with values from comp
+	vfc->Top.x = comp[0][3] - comp[0][1]; // see handout to fill in with values from comp
+	vfc->Top.y = comp[1][3] - comp[1][1]; // see handout to fill in with values from comp
+	vfc->Top.z = comp[2][3] - comp[2][1]; // see handout to fill in with values from comp
+	vfc->Top.w = comp[3][3] - comp[3][1]; // see handout to fill in with values from comp
 	vfc->planes[3] = vfc->Top;
 	//cout << "Top " << Top.x << " " << Top.y << " " << Top.z << " " << Top.w << endl;
 
-	vfc->Near.x = 0; // see handout to fill in with values from comp
-	vfc->Near.y = 0; // see handout to fill in with values from comp
-	vfc->Near.z = 0; // see handout to fill in with values from comp
-	vfc->Near.w = 0; // see handout to fill in with values from comp
+	vfc->Near.x = comp[0][3] + comp[0][2]; // see handout to fill in with values from comp
+	vfc->Near.y = comp[1][3] + comp[1][2]; // see handout to fill in with values from comp
+	vfc->Near.z = comp[2][3] + comp[2][2]; // see handout to fill in with values from comp
+	vfc->Near.w = comp[3][3] + comp[3][2]; // see handout to fill in with values from comp
 	vfc->planes[4] = vfc->Near;
 	//cout << "Near " << Near.x << " " << Near.y << " " << Near.z << " " << Near.w << endl;
 
-	vfc->Far.x = 0; // see handout to fill in with values from comp
-	vfc->Far.y = 0; // see handout to fill in with values from comp
-	vfc->Far.z = 0; // see handout to fill in with values from comp
-	vfc->Far.w = 0; // see handout to fill in with values from comp
+	vfc->Far.x = comp[0][3] - comp[0][2]; // see handout to fill in with values from comp
+	vfc->Far.y = comp[1][3] - comp[1][2]; // see handout to fill in with values from comp
+	vfc->Far.z = comp[2][3] - comp[2][2]; // see handout to fill in with values from comp
+	vfc->Far.w = comp[3][3] - comp[3][2]; // see handout to fill in with values from comp
 	vfc->planes[5] = vfc->Far;
 	//cout << "Far " << Far.x << " " << Far.y << " " << Far.z << " " << Far.w << endl;
 	return vfc;
@@ -85,14 +85,14 @@ int VFC::ViewFrustCull(VFC* vfc, vec3 center, float radius, bool cull) {
 	float dist;
 
 	if (cull) {
-		cout << "testing against all planes" << endl;
+		//cout << "testing against all planes" << endl;
 		for (int i = 0; i < 6; i++) {
 			dist = DistToPlane(vfc->planes[i].x, vfc->planes[i].y, vfc->planes[i].z, vfc->planes[i].w, center);
 			//test against each plane
 			if (dist < 0) {
+				cout << "object culled" << endl;
 				return 1;
 			}
-
 		}
 		return 0;
 	}
