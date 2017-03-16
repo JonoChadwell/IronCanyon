71e7ec45c44b779ed47216f8244600670245af15
Jono Chadwell <Jono.Chadwell@gmail.com>
2017-03-15 23:13:56 -0700

    Added more wave definitions and flavor text w/ gui. I hope this doesn't cause to much extra work Evan :/

--- IronCanyon/EnemySpawner.cpp
+++ IronCanyon/EnemySpawner.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <glm/vec3.hpp>
 #include <algorithm>
+#include <string>
 #include <cmath>
 
 #define SPAWN_AREA 500.0f
@@ -29,6 +30,7 @@ struct enemy_spawn_info {
 };
 
 struct wave_spawn_info {
+    const char* flavor;
     float waveDuration;
     vector<enemy_spawn_info> spawns;
 };
@@ -37,6 +39,7 @@ vector<wave_spawn_info> waves =
 {
     // wave 1
     {
+        "Incoming!",
         15.0f,
         {
             {FLOATER, 6.0f, 20.0f, 5},
@@ -44,12 +47,93 @@ vector<wave_spawn_info> waves =
     },
     //wave 2
     {
-        20.0f,
+        "Scanners show bigger enemies on the horizon.",
+        30.0f,
         {
-            {FLOATER, 8.0f, 20.0f, 5},
-            {FLOATER, 8.0f, 18.0f, 5},
-            {FLOATER, 8.0f, 16.0f, 5},
-            {FLOATER, 8.0f, 14.0f, 5},
+            { FLOATER, 7.0f, 30.0f, 3 },
+            { FLOATER, 7.0f, 28.0f, 3 },
+            { FLOATER, 7.0f, 26.0f, 3 },
+            { FLOATER, 7.0f, 24.0f, 3 },
+        }
+    },
+    //wave 3
+    {
+        "Watch out these enemies have phasors.",
+        30.0f,
+        {
+            { WALKER, 5.0f, 30.0f, 15 },
+        }
+    },
+    //wave 4
+    {
+        "Big wave coming after this, better heal up.",
+        50.0f,
+        {
+            { WALKER, 5.0f, 50.0f, 5 },
+            { FLOATER, 9.0f, 50.0f, 10 },
+        }
+    },
+    //wave 5
+    {
+        "Here they come!",
+        60.0f,
+        {
+            { FLOATER, 8.0f, 60.0f, 15 },
+            { FLOATER, 8.5f, 59.0f, 15 },
+            { FLOATER, 9.0f, 58.0f, 15 },
+            { FLOATER, 9.5f, 57.0f, 15 },
+        }
+    },
+    //wave 6
+    {
+        "Are the enemies getting faster?",
+        40.0f,
+        {
+            { WALKER, 7.0f, 40.0f, 8 },
+            { FLOATER, 12.0f, 40.0f, 14 },
+        }
+    },
+    //wave 7
+    {
+        "Yes. They definitly are getting faster.",
+        40.0f,
+        {
+            { WALKER, 10.0f, 40.0f, 10 },
+            { FLOATER, 14.0f, 40.0f, 14 },
+        }
+    },
+    //wave 8
+    {
+        "SLOW DOWN DAMNIT!",
+        40.0f,
+        {
+            { WALKER, 13.0f, 40.0f, 10 },
+            { FLOATER, 20.0f, 40.0f, 15 },
+        }
+    },
+    //wave 9
+    {
+        "Another big wave after this.",
+        60.0f,
+        {
+            { WALKER, 8.0f, 60.0f, 5 },
+            { FLOATER, 15.0f, 60.0f, 10 },
+        }
+    },
+    //wave 10
+    {
+        "I hope you have some turrets by now.",
+        120.0f,
+        {
+            { WALKER, 6.0f, 120.0f, 10 },
+            { WALKER, 7.0f, 110.0f, 10 },
+            { WALKER, 8.0f, 100.0f, 10 },
+            { WALKER, 9.0f, 90.0f, 10 },
+            { WALKER, 10.0f, 80.0f, 10 },
+            { WALKER, 11.0f, 70.0f, 10 },
+            { WALKER, 12.0f, 60.0f, 10 },
+            { WALKER, 13.0f, 50.0f, 10 },
+            { WALKER, 14.0f, 40.0f, 10 },
         }
     },
 };
@@ -66,10 +150,11 @@ namespace {
             return waves[wave];
         }
         wave_spawn_info rtn = {
+            "(im all out of flavor text)",
             30.0f,
             {
-                {FLOATER, 10.0f, 30.0f, 10 + wave},
-                {WALKER, 5.0f, 30.0f, 5 + wave},
+                {FLOATER, 15.0f, 30.0f, 10 + wave},
+                {WALKER, 8.0f, 30.0f, 5 + wave},
             }
         };
         return rtn;
@@ -82,7 +167,9 @@ EnemySpawner::EnemySpawner(Grid* grid, Player* player) :
     nextWaveTimer(1.0f),
     waveNumber(0),
     active(false),
-    wave({})
+    wave({}),
+    flavorText("Welcome to Iron Canyon!"),
+    flavorTextDisplayTime(5.0)
 {
 }
 
@@ -111,9 +198,11 @@ vector<GameObject*> EnemySpawner::update(float dt)
     nextWaveTimer -= dt;
     if (nextWaveTimer <= 0.0f)
     {
+        flavorTextDisplayTime = 5.0;
         wave_spawn_info wi = generateWave(waveNumber);
         nextWaveTimer = wi.waveDuration;
         wave = wi.spawns;
+        flavorText = wi.flavor;
         waveNumber++;
     }
 
